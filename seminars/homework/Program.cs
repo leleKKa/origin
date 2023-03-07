166604c4321976aecb1d18558b57b2fde3cecadd
Olga <oadotik@gmail.com>
2023-03-07 23:13:04 +0300

    поправка

--- seminars/homework/Program.cs
+++ seminars/homework/Program.cs
@@ -16,3 +16,25 @@ while (max < num2)
   Console.WriteLine("min = "+ num1);
 }
 */
+
+// Задача 4: Напишите программу, которая принимает на вход три 
+// числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Input a first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a third number");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if(num1>max)
+   max = num1;
+if(num2>max)
+  max = num2;
+if(num3>max)
+  max = num3;
+
+Console.WriteLine("max = " + max);
+

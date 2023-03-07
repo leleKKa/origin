2ca5ac894bb49f78ad5d2a14f1ce79c50fb7daf2
Olga <oadotik@gmail.com>
2023-03-07 19:22:22 +0300

    маша

---
+++ examples/example006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter username");
+string username == Console.ReadLine();
+if(username == "Masha");
+{
+  Console.WriteLine("Ура, это же Masha");
+}
+else
+{
+  Console.Write("Hello, ");
+  Console.WriteLine(username);
+}

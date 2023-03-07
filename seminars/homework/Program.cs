87678baae385f596f82b4a998a41fbf9196bc10a
Olga <oadotik@gmail.com>
2023-03-07 22:46:06 +0300

    gпервая задача

--- seminars/homework/Program.cs
+++ seminars/homework/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Input a first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+while (max < num2)
+{
+  Console.WriteLine(max = num2);
+  Console.Write(max);
+}

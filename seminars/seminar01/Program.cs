2fb5040be27f1e7ffed83153778fb16bf6e64a1d
Olga <oadotik@gmail.com>
2023-03-07 22:11:13 +0300

    теория

--- seminars/seminar01/Program.cs
+++ seminars/seminar01/Program.cs
@@ -6,8 +6,59 @@
 
 // bool check = true;
 
-int num1 = 5;
-int num2 = 10;
+// int num1 = 5;
+// int num2 = 10;
 
 // Console.WriteLine("My number are " + num1 + " and " + num2);
-Console.WriteLine($"My number are {num1} and {num2}");
+// Console.WriteLine($"My numbers are {num1} and {num2}");
+
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Напишите программу? которая на вход принимает два числа и проверяет? является 
+// ли первое число квадратом второго.
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+
+if(num1 == quad2)
+{
+  Console.WriteLine("Yes!");
+}
+else
+{
+  Console.WriteLine("No!");
+}
+*/
+
+// Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+/*
+Console.Write("Input a positive number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = -number;
+
+while (current <= number)
+{
+  Console.Write(current + " ");
+  current++; // current = current + 1
+  // current += b;
+}
+*/
+
+// 32143 / 10 = 3214
+// 32143 / 100 = 321
+// 32142 / 1000 = 32
+// 32142 / 10000 = 3
+// 32143 % 10 = 3
+// 32143 % 100 = 43
+
+// 32143 -> 21 /100%100
+
+
+

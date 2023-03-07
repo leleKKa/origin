266e5888a474c025f5984f90eb3fba19f392d959
Olga <oadotik@gmail.com>
2023-03-08 00:02:25 +0300

    3задача

--- seminars/homework/Program.cs
+++ seminars/homework/Program.cs
@@ -56,9 +56,9 @@ int current = num % 2;
 
 if(current == 0)
   Console.WriteLine("четное число");
-if(current > 0)
-  Console.WriteLine("число нечетное");
-  */
+else
+  Console.WriteLine("нечетное число");
+*/
 
   // Задача 8: Напишите программу, которая на вход принимает число (N), 
   // а на выходе показывает все чётные числа от 1 до N.

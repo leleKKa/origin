02c6bcbbbfa3553d94a593ec8f550d056ede2909
Olga <oadotik@gmail.com>
2023-03-07 23:37:08 +0300

    поправка

--- seminars/homework/Program.cs
+++ seminars/homework/Program.cs
@@ -20,6 +20,7 @@ while (max < num2)
 // Задача 4: Напишите программу, которая принимает на вход три 
 // числа и выдаёт максимальное из этих чисел.
 
+/*
 Console.WriteLine("Input a first number");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input a second number");
@@ -37,4 +38,19 @@ if(num3>max)
   max = num3;
 
 Console.WriteLine("max = " + max);
+*/
+
+// Задача 6: Напишите программу, которая на вход принимает число 
+//и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Input a number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = num % 2;
+
+if(current == 0)
+  Console.WriteLine("четное число");
+if(current > 0)
+  Console.WriteLine("число нечетное");
+  
 

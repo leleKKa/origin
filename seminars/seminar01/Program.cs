683bd8b7c0fb6f7683b91edc98e773dbc243958d
Olga <oadotik@gmail.com>
2023-03-07 20:45:30 +0300

    save

--- seminars/seminar01/Program.cs
+++ seminars/seminar01/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// int integerNum = 34;
+// double doubleNum = 2.344;
+
+// string word = "Hello!";
+// char symbol = 'f';
+
+// bool check = true;
+
+int num1 = 5;
+int num2 = 10;
+
+// Console.WriteLine("My number are " + num1 + " and " + num2);
+Console.WriteLine($"My number are {num1} and {num2}");

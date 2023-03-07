06e96bbe222ade3515138744bc93812000685610
Olga <oadotik@gmail.com>
2023-03-07 22:51:31 +0300

    поправка

--- seminars/homework/Program.cs
+++ seminars/homework/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Input a first number");
+﻿// Задача 2: Напишите программу, которая на вход принимает 
+// два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Input a first number");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input a second number");
 int num2 = Convert.ToInt32(Console.ReadLine());
@@ -8,5 +11,6 @@ int max = num1;
 while (max < num2)
 {
   Console.WriteLine(max = num2);
-  Console.Write(max);
+  Console.WriteLine("max = " + num2);
+  Console.WriteLine("min = "+ num1);
 }

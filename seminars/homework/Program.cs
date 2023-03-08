815807386f6f1c6378f30a31e5d56c6a149cf7ba
Olga <oadotik@gmail.com>
2023-03-08 17:45:19 +0300

    4задача

--- seminars/homework/Program.cs
+++ seminars/homework/Program.cs
@@ -63,4 +63,24 @@ else
   // Задача 8: Напишите программу, которая на вход принимает число (N), 
   // а на выходе показывает все чётные числа от 1 до N.
 
+Console.Write("Input a positive number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+
+while(current <= number)
+{
+      if(current % 2 == 0)
+      {
+        Console.Write(current + " ");
+         current++;
+      }
+      else
+      current++;
+}
+
+
+
+
+
 
